2504e532645b63122d24138c611f397f0bbb5de1
jrauvola <59544392+jrauvola@users.noreply.github.com>
2021-03-19 15:18:27 +0200

    Created Delete and generating id functionality

--- sample_backend.py
+++ sample_backend.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+import string
+import random
+
 
 app = Flask(__name__) 
 CORS(app)
+
 
 @app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
@@ -18,6 +22,8 @@ def get_user(id):
            if user['id'] == id:
                users['users_list'].pop(user)
    return users
+
+
 
 users = { 
    'users_list' :
@@ -51,7 +57,9 @@ users = {
 }
 
 def gen_id():
-  return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
+   x = ''.join([random.choice(string.ascii_letters) for n in range (3)])
+   y = ''.join([random.choice(string.digits) for n in range(3)])
+   return  x + y
 
 @app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
@@ -66,7 +74,9 @@ def get_users():
       return users
    elif request.method == 'POST':
       userToAdd = request.get_json()
+      print(userToAdd)
       userToAdd['id'] = gen_id()
+      print(gen_id())
       users['users_list'].append(userToAdd)
       resp = jsonify(success=True, user = userToAdd)
       resp.status_code = 201 #optionally, you can always set a response code. 
@@ -75,12 +85,12 @@ def get_users():
    elif request.method == 'DELETE':
       userToDelete = request.get_json()
       for user in users['users_list']:
-           if user['id'] == userToDelete['id]:
-               users['users_list'].remove(user)
-               resp = jsonify(success=True)
-               resp.status_code = 200
-               return resp
-      resp = jsonify(success=False)
+         if user['id'] == userToDelete['id'] and user['name'] == userToDelete['name']:
+            users['users_list'].remove(user)
+            resp = jsonify(success=True)
+            resp.status_code = 200
+            return resp
+      return jsonify(success=False)
       
                 
                
